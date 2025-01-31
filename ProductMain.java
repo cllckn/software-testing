f43b6a414d7aa4e59c4185f22cdc4e298614b2e8
cllckn <orcait.en@gmail.com>
2025-01-31 15:42:09 +0500

    Add files via upload

---
+++ ProductMain.java
@@ -0,0 +1,8 @@
+package cc.oop.lecture14;
+
+public class ProductMain {
+    public static void main(String[] args) {
+        Product product = new Product(1, "Laptop", 1000.0, 10);
+        System.out.println(product);
+    }
+}
