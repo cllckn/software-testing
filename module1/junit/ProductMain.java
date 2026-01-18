a78de8971a02e64d1340df9bcbe1c541e9fd961c
cllckn <orcait.en@gmail.com>
2026-01-18 20:49:49 +0500

    Initialize Repository

--- module1/junit/ProductMain.java
+++
@@ -1,8 +0,0 @@
-package cc.oop.lecture14;
-
-public class ProductMain {
-    public static void main(String[] args) {
-        Product product = new Product(1, "Laptop", 1000.0, 10);
-        System.out.println(product);
-    }
-}
